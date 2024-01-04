996ef3b79978d82694a5469ef008aba26a67e9f4
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2024-01-04 15:34:41 +0530

    deploy script added

--- deploy/01-raffle-deploy.js
+++ deploy/01-raffle-deploy.js
@@ -11,54 +11,59 @@ const { verify } = require("../utils/verify")
     uint32 interval*/
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
-  const { deploy, log, get } = deployments
-  const { deployer } = await getNamedAccounts()
+ const { deploy, log, get } = deployments
+ const { deployer } = await getNamedAccounts()
 
-  const chainId = network.config.chainId
+ const chainId = network.config.chainId
 
-  const VRF_SUB_FUND_AMOUNT = ethers.parseEther("3")
+ const VRF_SUB_FUND_AMOUNT = ethers.parseEther("3")
 
-  let vrfCoordinatorV2Address
-  let subscriptionId
+ let vrfCoordinatorV2Address
+ let subscriptionId
+ let VRFCoordinatorV2Mock
 
-  if (developmentChains.includes(network.name)) {
-    const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-    vrfCoordinatorV2Address = VRFCoordinatorV2Mock.target
-    const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
-    const transactionReceipt = await transactionResponse.wait(1)
-    // console.log(transactionReceipt)
-    subscriptionId = transactionReceipt.logs[0].args.subId
+ if (developmentChains.includes(network.name)) {
+  VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+  vrfCoordinatorV2Address = VRFCoordinatorV2Mock.target
+  const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+  const transactionReceipt = await transactionResponse.wait(1)
+  // console.log(transactionReceipt)
+  subscriptionId = transactionReceipt.logs[0].args.subId
 
-    await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
-  } else {
-    vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
-    subscriptionId = networkConfig[chainId].subscriptionId
-  }
+  await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+ } else {
+  vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+  subscriptionId = networkConfig[chainId].subscriptionId
+ }
 
-  const entranceFee = networkConfig[chainId].entranceFee
-  const gasLane = networkConfig[chainId].gasLane
-  const callbackGasLimit = networkConfig[chainId].callbackGasLimit
-  const interval = networkConfig[chainId].interval
+ const entranceFee = networkConfig[chainId].entranceFee
+ const gasLane = networkConfig[chainId].gasLane
+ const callbackGasLimit = networkConfig[chainId].callbackGasLimit
+ const interval = networkConfig[chainId].interval
 
-  const args = [
-    vrfCoordinatorV2Address,
-    entranceFee,
-    gasLane,
-    subscriptionId,
-    callbackGasLimit,
-    interval,
-  ]
-  const raffle = await deploy("Raffle", {
-    from: deployer,
-    log: true,
-    args: args,
-    waitConfirmations: network.config.blockConfirmations || 1,
-  })
-  if (!developmentChains.includes(network.chain)) {
-    log("verifyingg contract")
-    await verify(raffle.address, args)
-  }
-  log("----------------------------------")
+ const args = [
+  vrfCoordinatorV2Address,
+  entranceFee,
+  gasLane,
+  subscriptionId,
+  callbackGasLimit,
+  interval,
+ ]
+
+ const raffle = await deploy("Raffle", {
+  from: deployer,
+  log: true,
+  args: args,
+  waitConfirmations: network.config.blockConfirmations || 1,
+ })
+ if (developmentChains.includes(network.name)) {
+  await VRFCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
+ }
+ if (!developmentChains.includes(network.name)) {
+  log("verifyingg contract")
+  await verify(raffle.address, args)
+ }
+ log("----------------------------------")
 }
 
 module.exports.tags = ["all", "raffle"]
