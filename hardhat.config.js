ab8fc5de79f10f9da77f45ab63a0d9e48cc04591
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2024-01-04 15:34:56 +0530

    bugs

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 require("hardhat-gas-reporter")
 require("hardhat-deploy")
 require("@nomiclabs/hardhat-ethers")
-require("@nomiclabs/hardhat-etherscan")
+require("@nomicfoundation/hardhat-toolbox")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
@@ -12,45 +12,48 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "0Xkey"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0Xkey"
 const COIN_MARKET_API_KEY = process.env.COIN_MARKET_API_KEY || "0Xkey"
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 11155111,
-      blockConfirmations: 3,
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545/",
-      chainId: 31337,
-    },
+ defaultNetwork: "hardhat",
+ networks: {
+  sepolia: {
+   url: SEPOLIA_RPC_URL,
+   accounts: [PRIVATE_KEY],
+   chainId: 11155111,
+   blockConfirmations: 3,
   },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-    user: {
-      default: 1,
-    },
+  localhost: {
+   url: "http://127.0.0.1:8545/",
+   chainId: 31337,
   },
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.8",
-      },
-      {
-        version: "0.6.6",
-      },
-    ],
+ },
+ namedAccounts: {
+  deployer: {
+   default: 0,
   },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+  user: {
+   default: 1,
   },
-  gasReporter: {
-    enabled: false,
-    outputFile: "gas-reporter.txt",
-    noColor: true,
-    currency: "USD",
-    coinmarketcap: COIN_MARKET_API_KEY,
-  },
+ },
+ solidity: {
+  compilers: [
+   {
+    version: "0.8.8",
+   },
+   {
+    version: "0.6.6",
+   },
+  ],
+ },
+ etherscan: {
+  apiKey: ETHERSCAN_API_KEY,
+ },
+ gasReporter: {
+  enabled: false,
+  outputFile: "gas-reporter.txt",
+  noColor: true,
+  currency: "USD",
+  coinmarketcap: COIN_MARKET_API_KEY,
+ },
+ mocha: {
+  timeout: 700000,
+ },
 }
